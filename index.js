e7c78cb2c289ec7ca479069455087761041831b1
tcm390 <60634884+tcm390@users.noreply.github.com>
2022-03-10 21:29:23 -0500

    add front dust

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 import metaversefile from 'metaversefile';
 const {useApp, useFrame, useLocalPlayer, useCameraManager, useLoaders, useInternals} = metaversefile;
 const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
-
+import ioManager from '../../io-manager';
 
 export default () => {
   const app = useApp();
@@ -11,6 +11,7 @@ export default () => {
   const cameraManager = useCameraManager();
   const {renderer, camera} = useInternals();
   let narutoRunTime=0; 
+  let lastStopSw=0;
   const textureLoader = new THREE.TextureLoader()
   const wave2 = textureLoader.load(`${baseUrl}/textures/wave2.jpeg`)
   const wave20 = textureLoader.load(`${baseUrl}/textures/wave20.png`)
@@ -19,6 +20,7 @@ export default () => {
   const textureG = textureLoader.load(`${baseUrl}/textures/g.jpg`);
   const textureB = textureLoader.load(`${baseUrl}/textures/b.jpg`);
   const electronicballTexture = textureLoader.load(`${baseUrl}/textures/electronic-ball2.png`);
+  const noiseMap = textureLoader.load(`${baseUrl}/textures/noise.jpg`);
     //################################################ trace narutoRun Time ########################################
     {
         useFrame(() => {
@@ -27,7 +29,7 @@ export default () => {
             //console.log(localPlayer.actionInterpolants.jump)
             if (localPlayer.hasAction('narutoRun')){
                     narutoRunTime++;
-                    
+                    lastStopSw=1;
                 }
                 else{
                     narutoRunTime=0;
@@ -2144,6 +2146,272 @@ export default () => {
             app.updateMatrixWorld();
         });
     }
+    //##################################### front dust ################################################
+    {
+        const particleCount = 100;
+        const group=new THREE.Group();
+        let info = {
+            velocity: [particleCount]
+        }
+        let acc = new THREE.Vector3(-0.000, 0, 0.0018);
+    
+        //##################################################### get Dust geometry #####################################################
+        const identityQuaternion = new THREE.Quaternion();
+        const _getDustGeometry = geometry => {
+            //console.log(geometry)
+            const geometry2 = new THREE.BufferGeometry();
+            ['position', 'normal', 'uv'].forEach(k => {
+              geometry2.setAttribute(k, geometry.attributes[k]);
+            });
+            geometry2.setIndex(geometry.index);
+            
+            const positions = new Float32Array(particleCount * 3);
+            const positionsAttribute = new THREE.InstancedBufferAttribute(positions, 3);
+            geometry2.setAttribute('positions', positionsAttribute);
+            const quaternions = new Float32Array(particleCount * 4);
+            for (let i = 0; i < particleCount; i++) {
+              identityQuaternion.toArray(quaternions, i * 4);
+            }
+            const quaternionsAttribute = new THREE.InstancedBufferAttribute(quaternions, 4);
+            geometry2.setAttribute('quaternions', quaternionsAttribute);
+    
+            const startTimes = new Float32Array(particleCount);
+            const startTimesAttribute = new THREE.InstancedBufferAttribute(startTimes, 1);
+            geometry2.setAttribute('startTimes', startTimesAttribute);
+    
+            const opacityAttribute = new THREE.InstancedBufferAttribute(new Float32Array(particleCount), 1);
+            opacityAttribute.setUsage(THREE.DynamicDrawUsage);
+            geometry2.setAttribute('opacity', opacityAttribute);
+    
+            const brokenAttribute = new THREE.InstancedBufferAttribute(new Float32Array(particleCount), 1);
+            brokenAttribute.setUsage(THREE.DynamicDrawUsage);
+            geometry2.setAttribute('broken', brokenAttribute);
+        
+            return geometry2;
+        };
+    
+        //##################################################### material #####################################################
+        let dustMaterial= new THREE.MeshToonMaterial();
+        dustMaterial.transparent=true; 
+        dustMaterial.depthWrite=false;
+        dustMaterial.alphaMap=noiseMap;
+        //dustMaterial.blending= THREE.AdditiveBlending;
+        //dustMaterial.side=THREE.DoubleSide;
+        //dustMaterial.opacity=0.2;
+    
+        const uniforms = {
+            uTime: {
+                value: 0
+            },
+        }
+        dustMaterial.onBeforeCompile = shader => {
+            shader.uniforms.uTime = uniforms.uTime;
+            shader.vertexShader = 'attribute float opacity;attribute float broken;\n varying float vOpacity; varying float vBroken; varying vec3 vPos; \n ' + shader.vertexShader;
+            shader.vertexShader = shader.vertexShader.replace(
+              '#include <begin_vertex>',
+              ['vec3 transformed = vec3( position );', 'vOpacity = opacity; vBroken = broken; vPos = position;'].join('\n')
+            );
+            shader.fragmentShader = 'uniform float uTime; varying float vBroken; varying float vOpacity; varying vec3 vPos;\n' + shader.fragmentShader;
+            shader.fragmentShader = shader.fragmentShader
+            .replace(
+                `vec4 diffuseColor = vec4( diffuse, opacity );`,
+                `
+                  vec4 diffuseColor = vec4( diffuse, vOpacity);
+      
+                `
+            );
+            shader.fragmentShader = shader.fragmentShader.replace(
+                '#include <alphamap_fragment>',
+                [
+                  'float broken = abs( sin( 1.0 - vBroken ) ) - texture2D( alphaMap, vUv ).g;',
+                  'if ( broken < 0.0001 ) discard;'
+                ].join('\n')
+            );
+        };
+        
+        //##################################################### load glb #####################################################
+        //let dustGeometry;
+        let dustApp;
+        (async () => {
+            const u = `${baseUrl}/assets/smoke.glb`;
+            dustApp = await new Promise((accept, reject) => {
+                const {gltfLoader} = useLoaders();
+                gltfLoader.load(u, accept, function onprogress() {}, reject);
+                
+            });
+            dustApp.scene.traverse(o => {
+              if (o.isMesh) {
+                addInstancedMesh(o.geometry);
+              }
+            });
+            
+    
+        })();
+    
+        
+    
+        //##################################################### object #####################################################
+        let mesh = null;
+        let dummy = new THREE.Object3D();
+    
+    
+        function addInstancedMesh(dustGeometry) {
+            const geometry = _getDustGeometry(dustGeometry);
+            mesh = new THREE.InstancedMesh(geometry, dustMaterial, particleCount);
+            group.add(mesh);
+            app.add(group);
+            setInstancedMeshPositions(mesh);
+            
+        }
+        let matrix = new THREE.Matrix4();
+        function setInstancedMeshPositions(mesh1) {
+            for (let i = 0; i < mesh1.count; i++) {
+                mesh.getMatrixAt(i, matrix);
+                dummy.scale.x = .00001;
+                dummy.scale.y = .00001;
+                dummy.scale.z = .00001;
+                dummy.position.x = (Math.random())*0.2;
+                dummy.position.y = -0.2;
+                dummy.position.z = i*0.1;
+                dummy.rotation.x=Math.random()*i;
+                dummy.rotation.y=Math.random()*i;
+                dummy.rotation.z=Math.random()*i;
+                info.velocity[i] = (new THREE.Vector3(
+                    0,
+                    0,
+                    -0.8-Math.random()));
+                info.velocity[i].divideScalar(20);
+                dummy.updateMatrix();
+                mesh1.setMatrixAt(i, dummy.matrix);
+            }
+            mesh1.instanceMatrix.needsUpdate = true;
+        }
+       
+    
+        let previousDirectionState=null;
+        let currentDirectionState=null;
+        let dum = new THREE.Vector3();
+        let originPoint = new THREE.Vector3(0,0,0);
+        let maxParticle=10;
+        let particleEmmitCount=-1;
+        useFrame(({timestamp}) => {
+    
+            if(ioManager.keys.right){
+                currentDirectionState='right';
+            }   
+            if(ioManager.keys.left){
+                currentDirectionState='left';
+            }  
+            if(ioManager.keys.up){
+                currentDirectionState='up';
+            }   
+            if(ioManager.keys.down){
+                currentDirectionState='down';
+            }
+            group.position.copy(localPlayer.position);
+            group.rotation.copy(localPlayer.rotation);
+            if (localPlayer.avatar) {
+              group.position.y -= localPlayer.avatar.height;
+              group.position.y += 0.2;
+            }
+            localPlayer.getWorldDirection(dum)
+            dum = dum.normalize();
+            group.position.x+=0.3*dum.x;
+            group.position.z+=0.3*dum.z;
+        
+            if((lastStopSw===1 && narutoRunTime===0 && particleEmmitCount===-1 )){
+                if(localPlayer.hasAction('fly') || localPlayer.hasAction('jump')){
+                    lastStopSw=0;
+                }
+                else{
+                    particleEmmitCount=0;
+                }
+                
+            } 
+            if (mesh) {
+                const opacityAttribute = mesh.geometry.getAttribute('opacity');
+                const brokenAttribute = mesh.geometry.getAttribute('broken');
+                const startTimesAttribute = mesh.geometry.getAttribute('startTimes');
+                for (let i = 0; i < particleCount; i++) {
+                    mesh.getMatrixAt(i, matrix);
+                    matrix.decompose(dummy.position, dummy.quaternion, dummy.scale);
+                    
+                    if (dummy.position.distanceTo(originPoint)>3 /*|| (timestamp-startTimesAttribute.getX(i)>900)*/) {
+                        startTimesAttribute.setX(i,timestamp);
+                        if(particleEmmitCount>maxParticle){
+                            lastStopSw=0;
+                            particleEmmitCount=-1;
+                            //maxParticle=15+Math.random()*10;
+                        }
+                        opacityAttribute.setX(i, 1);
+                        brokenAttribute.setX(i, Math.random()-0.8);
+                        if(particleEmmitCount!==-1 ){
+                            dummy.scale.x = 0.08+Math.random()*0.05;
+                            dummy.scale.y = 0.08+Math.random()*0.05;
+                            dummy.scale.z = 0.08+Math.random()*0.05;
+                            particleEmmitCount++;
+                        }
+                        else{
+                            dummy.scale.x = .00001;
+                            dummy.scale.y = .00001;
+                            dummy.scale.z = .00001;
+                            //opacityAttribute.setX(i, 0.01);
+                        }
+                        
+                        
+                        dummy.position.x = 0;
+                        dummy.position.y = -0.1;
+                        dummy.position.z = 0.;
+                        
+                        info.velocity[i].x=0;
+                        info.velocity[i].y=0;
+                        info.velocity[i].z=-0.8-Math.random();
+                        
+                            
+                        info.velocity[i].divideScalar(20);
+                        
+                    }
+                    
+                    opacityAttribute.setX(i, opacityAttribute.getX(i)-0.03);
+                    brokenAttribute.setX(i, brokenAttribute.getX(i)+0.035);
+                        
+                    dummy.rotation.x+=0.1*(Math.random()-0.5);
+                    dummy.rotation.y+=0.1*(Math.random()-0.5);
+                    dummy.rotation.z+=0.1*(Math.random()-0.5);
+                    
+                    dummy.scale.x*=1.03;
+                    dummy.scale.y*=1.03;
+                    dummy.scale.z*=1.03;
+                    
+                    
+                    if(narutoRunTime>0){
+                        dummy.scale.x = .00001;
+                        dummy.scale.y = .00001;
+                        dummy.scale.z = .00001;
+                    }
+                    if(previousDirectionState!==currentDirectionState){
+                        dummy.scale.x = .00001;
+                        dummy.scale.y = .00001;
+                        dummy.scale.z = .00001;
+                    }
+                    //acc.x=0.005*(Math.random()-0.5);
+                    //if(dummy.position.distanceTo(originPoint)>2.5 )
+                        info.velocity[i].add(acc);
+                    dummy.position.add(info.velocity[i]);
+                    dummy.updateMatrix();
+                    mesh.setMatrixAt(i, dummy.matrix);
+    
+                }
+                mesh.instanceMatrix.needsUpdate = true;
+                opacityAttribute.needsUpdate = true;
+                brokenAttribute.needsUpdate = true;
+                startTimesAttribute.needsUpdate = true;
+    
+            }
+            group.updateMatrixWorld();
+            previousDirectionState=currentDirectionState;
+        });
+      }
     //##################################### main ball ##################################################
     // {
         
